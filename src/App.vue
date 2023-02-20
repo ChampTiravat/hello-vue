c4b7840923c16703f2638e97508ffcb808177439
champtiravat <tiravat2016@gmail.com>
2023-02-20 10:25:16 +0700

    [INIT] Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+//
+</script>
+
+<template>
+<header>
+  <h1>Navbar</h1>
+</header>
+
+<main>
+  <h1>Hello</h1>
+</main>
+</template>
+
+<style scoped>
+header {
+  background: green;
+  color: white;
+  padding: 1em;
+  text-align: center;
+  display: block;
+  width: 100%;
+}
+
+main {
+  display: block;
+}
+</style>
